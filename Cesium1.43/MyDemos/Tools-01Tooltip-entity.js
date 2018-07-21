5c84b06525ed8a27aae70f6929afcaeaee2d765e
YanzheZhang <gishpuzyz@163.com>
2018-07-21 17:27:50 +0800

    添加Tooltip工具
    Signed-off-by: YanzheZhang <gishpuzyz@163.com>

---
+++ Cesium1.43/MyDemos/Tools-01Tooltip-entity.js
@@ -0,0 +1,65 @@
+﻿var TooltipCesium = (function () {
+    var isInit = false;
+    var viewer;
+    var labelEntity;
+
+    function _() { };
+
+    _.initTool = function (_viewer) {
+        if (isInit) { return; }
+        viewer = _viewer;
+        labelEntity = viewer.entities.add({
+            position: Cesium.Cartesian3.fromDegrees(0, 0),
+            label: {
+                text: '提示',
+                font: '15px sans-serif',
+                pixelOffset: new Cesium.Cartesian2(8, 8),//y大小根据行数和字体大小改变
+                horizontalOrigin: Cesium.HorizontalOrigin.LEFT,
+                showBackground: true,
+                backgroundColor: new Cesium.Color(0.165, 0.165, 0.165, 1.0)
+            }
+        });
+        labelEntity.show = false;
+        isInit = true;
+    }
+
+    _.setVisible = function (visible) {
+        if (!isInit) { return; }
+        labelEntity.show = visible ? true : false;
+    };
+
+    /*
+    position屏幕坐标
+    显示在椭球面
+    */
+    _.showAt = function (position, message) {
+        if (!isInit) { return; }
+        if (position && message) {
+            labelEntity.show = true;
+            var cartesian = viewer.camera.pickEllipsoid(position, viewer.scene.globe.ellipsoid);// 
+            if (cartesian) {
+                labelEntity.position = cartesian;
+                labelEntity.show = true;
+                labelEntity.label.text = message;
+            } else {
+                labelEntity.show = false;
+            }
+        }
+    };
+
+    /*
+    cartesian世界坐标
+    显示在真实位置
+    */
+    _.showAtCartesian = function (cartesian, message) {
+        if (!isInit) { return; }
+        if (cartesian && message) {
+            labelEntity.show = true;
+
+            labelEntity.position = cartesian;
+            labelEntity.show = true;
+            labelEntity.label.text = message;
+        }
+    };
+    return _;
+})();
---
+++ Cesium1.43/MyDemos/Tools-02Tooltip-div.js
@@ -0,0 +1,49 @@
+﻿var TooltipDiv = (function () {
+    var isInit = false;
+
+
+    function _() { };
+
+    _.initTool = function (frameDiv) {
+        if (isInit) { return; }
+
+        var div = document.createElement('DIV');
+        div.className = "tooltipdiv right";//
+
+        var arrow = document.createElement('DIV');
+        arrow.className = "tooltipdiv-arrow";
+        div.appendChild(arrow);
+
+        var title = document.createElement('DIV');
+        title.className = "tooltipdiv-inner";
+        div.appendChild(title);
+
+        this._div = div;
+        this._title = title;
+
+        frameDiv.appendChild(div);
+
+        isInit = true;
+    }
+
+    _.setVisible = function (visible) {
+        if (!isInit) { return; }
+        this._div.style.display = visible ? 'block' : 'none';
+    };
+
+    /*
+    position屏幕坐标
+    显示在屏幕上
+    */
+    _.showAt = function (position, message) {
+        if (!isInit) { return; }
+        if (position && message) {
+            this.setVisible(true);
+            this._title.innerHTML = message;
+            this._div.style.left = position.x + 10 + "px";
+            this._div.style.top = (position.y - this._div.clientHeight / 2) + "px";
+        }
+    };
+
+    return _;
+})();
