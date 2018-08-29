17c58fcfd6e48ccef1506332e1f03ed8fd646c1f
YanzheZhang <gishpuzyz@163.com>
2018-08-29 18:10:08 +0800

    添加Tools-21PrimitiveEllipse
    Signed-off-by: YanzheZhang <gishpuzyz@163.com>

---
+++ Cesium1.43/MyDemos/Tools-21EllipseGeometryLibraryEx.js
@@ -0,0 +1,136 @@
+﻿var EllipseGeometryLibraryEx = (function () {
+    var EllipseGeometryLibrary = {};
+
+    function pointOnEllipsoid(theta, rotation, northVec, eastVec, aSqr, ab, bSqr, mag, unitPos, result) {
+        var rotAxis = new Cesium.Cartesian3();
+        var tempVec = new Cesium.Cartesian3();
+        var unitQuat = new Cesium.Quaternion();
+        var rotMtx = new Cesium.Matrix3();
+
+        var azimuth = theta + rotation;
+
+        Cesium.Cartesian3.multiplyByScalar(eastVec, Math.cos(azimuth), rotAxis);
+        Cesium.Cartesian3.multiplyByScalar(northVec, Math.sin(azimuth), tempVec);
+        Cesium.Cartesian3.add(rotAxis, tempVec, rotAxis);
+
+        var cosThetaSquared = Math.cos(theta);
+        cosThetaSquared = cosThetaSquared * cosThetaSquared;
+
+        var sinThetaSquared = Math.sin(theta);
+        sinThetaSquared = sinThetaSquared * sinThetaSquared;
+
+        var radius = ab / Math.sqrt(bSqr * cosThetaSquared + aSqr * sinThetaSquared);
+        var angle = radius / mag;
+
+        // Create the quaternion to rotate the position vector to the boundary of the ellipse.
+        Cesium.Quaternion.fromAxisAngle(rotAxis, angle, unitQuat);
+        Cesium.Matrix3.fromQuaternion(unitQuat, rotMtx);
+
+        Cesium.Matrix3.multiplyByVector(rotMtx, unitPos, result);
+        Cesium.Cartesian3.normalize(result, result);
+        Cesium.Cartesian3.multiplyByScalar(result, mag, result);
+        return result;
+    }
+
+    /**
+     * Returns the positions raised to the given heights
+     * @private
+     */
+    EllipseGeometryLibrary.raisePositionsToHeight = function (positions, options, extrude) {
+        var scratchCartesian1 = new Cesium.Cartesian3();
+        var scratchCartesian2 = new Cesium.Cartesian3();
+        var scratchCartesian3 = new Cesium.Cartesian3();
+        var scratchNormal = new Cesium.Cartesian3();
+
+        var ellipsoid = options.ellipsoid;
+        var height = options.height;
+        var extrudedHeight = options.extrudedHeight;
+        var size = (extrude) ? positions.length / 3 * 2 : positions.length / 3;
+
+        var finalPositions = new Float64Array(size * 3);
+
+        var length = positions.length;
+        var bottomOffset = (extrude) ? length : 0;
+        for (var i = 0; i < length; i += 3) {
+            var i1 = i + 1;
+            var i2 = i + 2;
+
+            var position = Cesium.Cartesian3.fromArray(positions, i, scratchCartesian1);
+            ellipsoid.scaleToGeodeticSurface(position, position);
+
+            var extrudedPosition = Cesium.Cartesian3.clone(position, scratchCartesian2);
+            var normal = ellipsoid.geodeticSurfaceNormal(position, scratchNormal);
+            var scaledNormal = Cesium.Cartesian3.multiplyByScalar(normal, height, scratchCartesian3);
+            Cesium.Cartesian3.add(position, scaledNormal, position);
+
+            if (extrude) {
+                Cesium.Cartesian3.multiplyByScalar(normal, extrudedHeight, scaledNormal);
+                Cesium.Cartesian3.add(extrudedPosition, scaledNormal, extrudedPosition);
+
+                finalPositions[i + bottomOffset] = extrudedPosition.x;
+                finalPositions[i1 + bottomOffset] = extrudedPosition.y;
+                finalPositions[i2 + bottomOffset] = extrudedPosition.z;
+            }
+
+            finalPositions[i] = position.x;
+            finalPositions[i1] = position.y;
+            finalPositions[i2] = position.z;
+        }
+
+        return finalPositions;
+    };
+
+    /**
+    * options.semiMinorAxis：短半轴
+    * options.semiMajorAxis：长半轴
+    * options.rotation：旋转角度 弧度
+    * options.center：中心点 笛卡尔坐标
+    * options.granularity：粒度 弧度
+    * Returns an array of positions that make up the ellipse.
+    * @private
+    */
+    EllipseGeometryLibrary.computeEllipseEdgePositions = function (options) {
+        var unitPosScratch = new Cesium.Cartesian3();
+        var eastVecScratch = new Cesium.Cartesian3();
+        var northVecScratch = new Cesium.Cartesian3();
+        var scratchCartesian1 = new Cesium.Cartesian3();
+
+        var semiMinorAxis = options.semiMinorAxis;
+        var semiMajorAxis = options.semiMajorAxis;
+        var rotation = options.rotation;//法线
+        var center = options.center;
+        var granularity = options.granularity && (typeof options.granularity === "number") ? options.granularity : (Math.PI / 180.0);// 角度间隔
+        if (granularity > Math.PI / 12.0) { granularity = Math.PI / 12.0; }//最小分24
+        if (granularity < Math.PI / 180.0) { granularity = Math.PI / 180.0; }//最大分360
+        var aSqr = semiMinorAxis * semiMinorAxis;
+        var bSqr = semiMajorAxis * semiMajorAxis;
+        var ab = semiMajorAxis * semiMinorAxis;
+        var mag = Cesium.Cartesian3.magnitude(center);//
+        var unitPos = Cesium.Cartesian3.normalize(center, unitPosScratch);
+        var eastVec = Cesium.Cartesian3.cross(Cesium.Cartesian3.UNIT_Z, center, eastVecScratch);
+        eastVec = Cesium.Cartesian3.normalize(eastVec, eastVec);
+        var northVec = Cesium.Cartesian3.cross(unitPos, eastVec, northVecScratch);
+        var numPts = Math.ceil(Cesium.Math.PI*2 / granularity);
+        var deltaTheta = granularity;
+        var theta = 0;
+        
+        var position = scratchCartesian1;
+        var i;
+        var outerIndex = 0;
+        var outerPositions = [];
+        for (i = 0; i < numPts; i++) {
+            theta = i * deltaTheta;
+            position = pointOnEllipsoid(theta, rotation, northVec, eastVec, aSqr, ab, bSqr, mag, unitPos, position);
+            
+            outerPositions[outerIndex++] = position.x;
+            outerPositions[outerIndex++] = position.y;
+            outerPositions[outerIndex++] = position.z;
+        }
+
+        var r = {};
+        r.numPts = numPts;
+        r.outerPositions = outerPositions;
+        return r;
+    };
+    return EllipseGeometryLibrary;
+})();
---
+++ Cesium1.43/MyDemos/Tools-21PrimitiveEllipse.js
@@ -0,0 +1,220 @@
+﻿/*
+  绘制椭圆
+  当a=b时是圆（笛卡尔坐标系下正圆）
+*/
+var PrimitiveEllipse = (
+    function () {
+        var positionArr = [];
+        var colorArr = [];
+        var indiceArr = [];
+        var vertexShader;
+        var fragmentShader;
+        var geometry;
+        var appearance;
+        var viewer;
+        var ellipsoid = Cesium.Ellipsoid.WGS84;
+        function _(options) {
+            viewer = options.viewer;
+            vertexShader = VSPolylie();
+            fragmentShader = FSPolyline(); //Cartographic  Cartesian
+            if (options.center) {
+
+                var postionsTemp = [];
+                var colorsTemp = [];
+                var indicesTesm = [];
+                var height = options.height ? options.height:0 ;
+                var slices = options.slices && options.slices >= 36 ? options.slices : 360;// 
+                var semiMinorAxis = options.semiMinorAxis;
+                var semiMajorAxis = options.semiMajorAxis;
+                var rotation = Cesium.Math.toRadians(options.rotation);
+                var eopt = {};
+                eopt.semiMinorAxis = semiMinorAxis;
+                eopt.semiMajorAxis = semiMajorAxis;
+                eopt.rotation = rotation;
+                eopt.center = Cesium.Cartesian3.fromRadians(options.center.longitude, options.center.latitude, height);
+                eopt.granularity = Math.PI * 2.0 / parseFloat(slices);
+                var ellipse = EllipseGeometryLibraryEx.computeEllipseEdgePositions(eopt);
+                var raiseopt={};
+                raiseopt.ellipsoid=ellipsoid;
+                raiseopt.height=height;
+                raiseopt.extrudedHeight=0;
+
+                ellipse.outerPositions = EllipseGeometryLibraryEx.raisePositionsToHeight(ellipse.outerPositions, raiseopt, false);
+               
+                //上半部
+                for (var i = 0; i < ellipse.outerPositions.length; i = i + 3) {
+                    postionsTemp.push(ellipse.outerPositions[i]);
+                    postionsTemp.push(ellipse.outerPositions[i + 1]);
+                    postionsTemp.push(ellipse.outerPositions[i + 2]);
+
+                    colorsTemp.push(0.0);
+                    colorsTemp.push(0.0);
+                    colorsTemp.push(1.0);
+                    colorsTemp.push(1.0);
+                }
+                 
+                for (var i = 1; i < ellipse.outerPositions.length/ 3; i++) {
+                    indicesTesm.push(i - 1);
+                    indicesTesm.push(i);
+                }
+                //接边缝隙
+                //右缝隙
+                indicesTesm.push(0);
+                indicesTesm.push(ellipse.outerPositions.length / 3 -1);
+                 
+                positionArr = new Float64Array(postionsTemp);
+                colorArr = new Float32Array(colorsTemp);
+                indiceArr = new Uint16Array(indicesTesm);
+
+            } else {//if (options.CartographicCenter && options.CartographicPoint) {
+                var p1 = Cesium.Cartesian3.fromDegrees(0, 0, -10);
+                var p2 = Cesium.Cartesian3.fromDegrees(0, 0.001, -10);
+                positionArr = new Float64Array([
+                    p1.x, p1.y, p1.z,
+                    p2.x, p2.y, p2.z
+                ]);
+                colorArr = new Float32Array([
+                         0.0, 0.0, 1.0, 1.0,
+                         0.0, 0.0, 1.0, 1.0
+                ]);
+                indiceArr = new Uint16Array([0, 1]);
+            }
+
+            geometry = CreateGeometry(positionArr, colorArr, indiceArr);
+            appearance = CreateAppearence(fragmentShader, vertexShader);
+
+            this.primitive = viewer.scene.primitives.add(new Cesium.Primitive({
+                geometryInstances: new Cesium.GeometryInstance({
+                    geometry: geometry
+                }),
+                appearance: appearance,
+                asynchronous: false
+            }));
+        }
+
+        function CreateGeometry(positions, colors, indices) {
+            return new Cesium.Geometry({
+                attributes: {
+                    position: new Cesium.GeometryAttribute({
+                        componentDatatype: Cesium.ComponentDatatype.DOUBLE,
+                        componentsPerAttribute: 3,
+                        values: positions
+                    }),
+                    color: new Cesium.GeometryAttribute({
+                        componentDatatype: Cesium.ComponentDatatype.FLOAT,
+                        componentsPerAttribute: 4,
+                        values: colors
+                    })
+                },
+                indices: indices,
+                primitiveType: Cesium.PrimitiveType.LINES,
+                boundingSphere: Cesium.BoundingSphere.fromVertices(positions)
+            });
+        }
+
+        function CreateAppearence(fs, vs) {
+            return new Cesium.Appearance({
+                renderState: {
+                    blending: Cesium.BlendingState.PRE_MULTIPLIED_ALPHA_BLEND,  //混合
+                    depthTest: { enabled: true }, //深度测试
+                    depthMask: true
+                },
+                fragmentShaderSource: fs,
+                vertexShaderSource: vs
+            });
+        }
+
+        function VSPolylie() {
+            return "attribute vec3 position3DHigh;\
+            attribute vec3 position3DLow;\
+            attribute vec4 color;\
+            varying vec4 v_color;\
+            attribute float batchId;\
+            void main()\
+            {\
+                vec4 p = czm_computePosition();\
+                v_color =color;\
+                p = czm_modelViewProjectionRelativeToEye * p;\
+                gl_Position = p;\
+                gl_PointSize=10.0;\
+            }\
+            ";
+        }
+        function FSPolyline() {
+            return "varying vec4 v_color;\
+            void main()\
+            {\
+                gl_FragColor =v_color;\
+            }\
+            ";
+        }
+
+        _.prototype.remove = function () {
+            if (this.primitive != null) {
+                viewer.scene.primitives.remove(this.primitive);
+                this.primitive = null;
+            }
+        }
+
+        _.prototype.updatePosition = function (options) {
+            if (this.primitive != null) {
+                viewer.scene.primitives.remove(this.primitive);
+                var postionsTemp = [];
+                var colorsTemp = [];
+                var indicesTesm = [];
+                var height = options.height ? options.height : 0;
+                var slices = options.slices && options.slices >= 36 ? options.slices : 360;//默认72
+                var semiMinorAxis = options.semiMinorAxis;
+                var semiMajorAxis = options.semiMajorAxis;
+                var rotation = Cesium.Math.toRadians(options.rotation);
+                var eopt = {};
+                eopt.semiMinorAxis = semiMinorAxis;
+                eopt.semiMajorAxis = semiMajorAxis;
+                eopt.rotation = rotation;
+                eopt.center = Cesium.Cartesian3.fromRadians(options.center.longitude, options.center.latitude, height);
+                eopt.granularity = Math.PI * 2.0 / parseFloat(slices);
+                var ellipse = EllipseGeometryLibraryEx.computeEllipseEdgePositions(eopt);
+
+                var raiseopt = {};
+                raiseopt.ellipsoid = ellipsoid;
+                raiseopt.height = height;
+                raiseopt.extrudedHeight = 0;
+
+                ellipse.outerPositions = EllipseGeometryLibraryEx.raisePositionsToHeight(ellipse.outerPositions, raiseopt, false);
+
+                for (var i = 0; i < ellipse.outerPositions.length; i = i + 3) {
+                    postionsTemp.push(ellipse.outerPositions[i]);
+                    postionsTemp.push(ellipse.outerPositions[i + 1]);
+                    postionsTemp.push(ellipse.outerPositions[i + 2]);
+
+                    colorsTemp.push(0.0);
+                    colorsTemp.push(0.0);
+                    colorsTemp.push(1.0);
+                    colorsTemp.push(1.0);
+                }
+                for (var i = 1; i < ellipse.outerPositions.length / 3; i++) {
+                    indicesTesm.push(i - 1);
+                    indicesTesm.push(i);
+                }
+                 
+                //右缝隙
+                indicesTesm.push(0);
+                indicesTesm.push(ellipse.outerPositions.length / 3-1);
+                 
+                positionArr = new Float64Array(postionsTemp);
+                colorArr = new Float32Array(colorsTemp);
+                indiceArr = new Uint16Array(indicesTesm);
+                geometry = CreateGeometry(positionArr, colorArr, indiceArr);
+
+                this.primitive = viewer.scene.primitives.add(new Cesium.Primitive({
+                    geometryInstances: new Cesium.GeometryInstance({
+                        geometry: geometry
+                    }),
+                    appearance: appearance,
+                    asynchronous: false
+                }));
+            } else { return; }
+        }
+         
+        return _;
+    })();
