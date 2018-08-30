3940d5d54280c7364bd7d81ba43354f14f629a70
YanzheZhang <gishpuzyz@163.com>
2018-08-30 09:01:52 +0800

    修改Tools-20PrimitiveWaterface
    Signed-off-by: YanzheZhang <gishpuzyz@163.com>

--- Cesium1.43/MyDemos/Tools-20PrimitiveWaterFace.js
+++ Cesium1.43/MyDemos/Tools-20PrimitiveWaterFace.js
@@ -64,33 +64,33 @@
         }
          
         function FSWaterFace() {
-            return
-       'varying vec3 v_positionMC;\
-        varying vec3 v_positionEC;\
-        varying vec2 v_st;\
-        void main()\
-        {\
-            czm_materialInput materialInput;\
-            vec3 normalEC = normalize(czm_normal3D * czm_geodeticSurfaceNormal(v_positionMC, vec3(0.0), vec3(1.0)));\
-#ifdef FACE_FORWARD\
-                           normalEC = faceforward(normalEC, vec3(0.0, 0.0, 1.0), -normalEC);\
-#endif\
-       materialInput.s = v_st.s;\
-       materialInput.st = v_st;\
-       materialInput.str = vec3(v_st, 0.0);\
-       materialInput.normalEC = normalEC;\
-       materialInput.tangentToEyeMatrix = czm_eastNorthUpToEyeCoordinates(v_positionMC, materialInput.normalEC);\
-       vec3 positionToEyeEC = -v_positionEC;\
-       materialInput.positionToEyeEC = positionToEyeEC;\
-       czm_material material = czm_getMaterial(materialInput);\
-#ifdef FLAT\
-       gl_FragColor = vec4(material.diffuse + material.emission, material.alpha);\
-#else\
-       gl_FragColor = czm_phong(normalize(positionToEyeEC), material);\
-       gl_FragColor.a=0.5;\
-#endif\
-}\
-              ';
+            return 'varying vec3 v_positionMC;\n\
+varying vec3 v_positionEC;\n\
+varying vec2 v_st;\n\
+\n\
+void main()\n\
+{\n\
+    czm_materialInput materialInput;\n\
+    vec3 normalEC = normalize(czm_normal3D * czm_geodeticSurfaceNormal(v_positionMC, vec3(0.0), vec3(1.0)));\n\
+#ifdef FACE_FORWARD\n\
+    normalEC = faceforward(normalEC, vec3(0.0, 0.0, 1.0), -normalEC);\n\
+#endif\n\
+    materialInput.s = v_st.s;\n\
+    materialInput.st = v_st;\n\
+    materialInput.str = vec3(v_st, 0.0);\n\
+    materialInput.normalEC = normalEC;\n\
+    materialInput.tangentToEyeMatrix = czm_eastNorthUpToEyeCoordinates(v_positionMC, materialInput.normalEC);\n\
+    vec3 positionToEyeEC = -v_positionEC;\n\
+    materialInput.positionToEyeEC = positionToEyeEC;\n\
+    czm_material material = czm_getMaterial(materialInput);\n\
+#ifdef FLAT\n\
+    gl_FragColor = vec4(material.diffuse + material.emission, material.alpha);\n\
+#else\n\
+    gl_FragColor = czm_phong(normalize(positionToEyeEC), material);\n\
+    gl_FragColor.a = 0.5;\n\
+#endif\n\
+}\n\
+';
         }
 
         _.prototype.remove = function () {
