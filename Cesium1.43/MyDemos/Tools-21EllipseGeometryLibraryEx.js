71eebf69726407fcd7c28bfcfd6ef38748454475
YanzheZhang <gishpuzyz@163.com>
2018-08-31 22:34:21 +0800

    添加Tools-22PrimitiveSector 绘制扇形
    Signed-off-by: YanzheZhang <gishpuzyz@163.com>

--- Cesium1.43/MyDemos/Tools-21EllipseGeometryLibraryEx.js
+++ Cesium1.43/MyDemos/Tools-21EllipseGeometryLibraryEx.js
@@ -132,5 +132,60 @@
         r.outerPositions = outerPositions;
         return r;
     };
+
+    /**
+    * options.semiMinorAxis：短半轴
+    * options.semiMajorAxis：长半轴
+    * options.rotation：旋转角度 弧度
+    * options.center：中心点 笛卡尔坐标
+    * options.granularity：粒度 弧度
+    * options.angle：角度 弧度
+    * Returns an array of positions that make up the ellipse.
+    * @private
+    */
+    EllipseGeometryLibrary.computeSectorEdgePositions = function (options) {
+        var unitPosScratch = new Cesium.Cartesian3();
+        var eastVecScratch = new Cesium.Cartesian3();
+        var northVecScratch = new Cesium.Cartesian3();
+        var scratchCartesian1 = new Cesium.Cartesian3();
+
+        var semiMinorAxis = options.semiMinorAxis;
+        var semiMajorAxis = options.semiMajorAxis;
+        var rotation = options.rotation;
+        var angle = options.angle ? options.angle : Math.PI * 2.0;
+        var center = options.center;
+        var granularity = options.granularity && (typeof options.granularity === "number") ? options.granularity : (Math.PI / 180.0);// 角度间隔
+        if (granularity > Math.PI / 12.0) { granularity = Math.PI / 12.0; }//最小分24
+        if (granularity < Math.PI / 180.0) { granularity = Math.PI / 180.0; }//最大分360
+        var aSqr = semiMinorAxis * semiMinorAxis;
+        var bSqr = semiMajorAxis * semiMajorAxis;
+        var ab = semiMajorAxis * semiMinorAxis;
+        var mag = Cesium.Cartesian3.magnitude(center);//
+        var unitPos = Cesium.Cartesian3.normalize(center, unitPosScratch);
+        var eastVec = Cesium.Cartesian3.cross(Cesium.Cartesian3.UNIT_Z, center, eastVecScratch);
+        eastVec = Cesium.Cartesian3.normalize(eastVec, eastVec);
+        var northVec = Cesium.Cartesian3.cross(unitPos, eastVec, northVecScratch);
+        var numPts = Math.ceil(angle / granularity);//Math.ceil(Cesium.Math.PI * 2 / granularity);
+        var deltaTheta = granularity;
+        var theta = 0;
+
+        var position = scratchCartesian1;
+        var i;
+        var outerIndex = 0;
+        var outerPositions = [];
+        for (i = 0; i < numPts; i++) {
+            theta = i * deltaTheta;
+            position = pointOnEllipsoid(theta, rotation, northVec, eastVec, aSqr, ab, bSqr, mag, unitPos, position);
+
+            outerPositions[outerIndex++] = position.x;
+            outerPositions[outerIndex++] = position.y;
+            outerPositions[outerIndex++] = position.z;
+        }
+
+        var r = {};
+        r.numPts = numPts;
+        r.outerPositions = outerPositions;
+        return r;
+    };
     return EllipseGeometryLibrary;
 })();
