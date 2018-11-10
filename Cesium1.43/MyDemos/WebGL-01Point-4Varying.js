5f74ab94c5cdedf952e0a88cc8ad6d20865b6e87
YanzheZhang <gishpuzyz@163.com>
2018-11-10 22:05:26 +0800

    添加WebGL-01Point-4Varying
    Signed-off-by: YanzheZhang <gishpuzyz@163.com>

---
+++ Cesium1.43/MyDemos/WebGL-01Point-4Varying.js
@@ -0,0 +1,61 @@
+﻿var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'varying vec4 v_Position;\n' + //varying 变量
+  'void main() {\n' +
+  '  gl_Position = a_Position;\n' +
+  '  gl_PointSize = 10.0;\n' +
+  '  v_Position = gl_Position;\n' +// 将数据传入片元着色器
+  '}\n';
+
+var FSHADER_SOURCE =
+  'precision mediump float;\n' +
+  'varying vec4 v_Position;\n' +//对应声明varying 变量 接收顶点着色器传入数据
+  'void main() {\n' +
+  '  if(v_Position.x < 0.0) {\n' +
+  '    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+  '  } else { gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0); }\n' +
+  '}\n';
+
+function main() {
+    var canvas = document.getElementById('webgl');
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    canvas.onmousedown = function (ev) { click(ev, gl, canvas, a_Position) };
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+var g_points = []; 
+var g_colors = []; 
+function click(ev, gl, canvas, a_Position) {
+    var x = ev.clientX; 
+    var y = ev.clientY; 
+    var rect = ev.target.getBoundingClientRect();
+    x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
+    y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+    g_points.push([x, y]);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var len = g_points.length;
+    for (var i = 0; i < len; i++) {
+        var xy = g_points[i];
+        gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
+}
