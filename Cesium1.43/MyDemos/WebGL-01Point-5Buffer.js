fab233972d24510ced528eb91f93b5006866c570
YanzheZhang <gishpuzyz@163.com>
2018-11-10 22:06:41 +0800

    添加WebGL-01Point-5Buffer
    Signed-off-by: YanzheZhang <gishpuzyz@163.com>

---
+++ Cesium1.43/MyDemos/WebGL-01Point-5Buffer.js
@@ -0,0 +1,94 @@
+﻿var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'attribute vec4 a_Color;\n' +
+  'varying vec4 v_Color;\n' + 
+  'void main() {\n' +
+  '  gl_Position = a_Position;\n' +
+  '  gl_PointSize = 10.0;\n' +
+  '  v_Color = a_Color;\n' +  
+  '}\n';
+
+var FSHADER_SOURCE =
+  'precision mediump float;\n' + 
+  'varying vec4 v_Color;\n' +    
+  'void main() {\n' +
+  '  gl_FragColor = v_Color;\n' +
+  '}\n';
+
+function main() {
+    var canvas = document.getElementById('webgl');
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    //初始化顶点位置 类型化数组
+    var vertices = new Float32Array([
+       0.2, 0.4,
+       -0.5, 0.2,
+       0.8, -0.3
+    ]);
+    var n = 3; // 顶点个数
+    //初始化顶点颜色 类型化数组
+    var colors = new Float32Array([
+        0.2, 1.0, 0.0,
+        0.5, 0.0, 1.0,
+        0.8, 0.0, 0.0,
+    ]);
+
+    Draw(gl, n, vertices, colors);
+}
+
+function Draw(gl, n, vertices, colors) {
+    // 创建缓冲区对象
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+    // 将缓冲区对象绑定到目标
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // 向缓冲区对象写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    // 获取 attribute变量存储位置
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return -1;
+    }
+    // 将缓冲区对象分配给 a_Position
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    // 解除绑定
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    // 连接 a_Position与分配给它的缓冲区对象
+    gl.enableVertexAttribArray(a_Position);
+
+
+    //------------处理颜色缓冲区 
+    var colorBuffer = gl.createBuffer();
+    if (!colorBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+
+    var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+    if (a_Color < 0) {
+        console.log('Failed to get the storage location of a_Color');
+        return -1;
+    }
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.enableVertexAttribArray(a_Color);
+
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, n);
+}
