f60d0c02327d08bbb356ed8048f5a18c33d1b953
YanzheZhang <gishpuzyz@163.com>
2018-11-29 14:07:56 +0800

    添加WebGL-06Shadow-high
    Signed-off-by: YanzheZhang <gishpuzyz@163.com>

---
+++ Cesium1.43/MyDemos/WebGL-06Shadow-high.js
@@ -0,0 +1,255 @@
+﻿var SHADOW_VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'uniform mat4 u_MvpMatrix;\n' +
+  'void main() {\n' +
+  '  gl_Position = u_MvpMatrix * a_Position;\n' +
+  '}\n';
+var SHADOW_FSHADER_SOURCE =
+  '#ifdef GL_ES\n' +
+  'precision mediump float;\n' +
+  '#endif\n' +
+  'void main() {\n' +
+  '  const vec4 bitShift = vec4(1.0, 256.0, 256.0 * 256.0, 256.0 * 256.0 * 256.0);\n' +
+  '  const vec4 bitMask = vec4(1.0/256.0, 1.0/256.0, 1.0/256.0, 0.0);\n' +
+  '  vec4 rgbaDepth = fract(gl_FragCoord.z * bitShift);\n' + 
+  '  rgbaDepth -= rgbaDepth.gbaa * bitMask;\n' +
+  '  gl_FragColor = rgbaDepth;\n' +
+  '}\n';
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'attribute vec4 a_Color;\n' +
+  'uniform mat4 u_MvpMatrix;\n' +
+  'uniform mat4 u_MvpMatrixFromLight;\n' +
+  'varying vec4 v_PositionFromLight;\n' +
+  'varying vec4 v_Color;\n' +
+  'void main() {\n' +
+  '  gl_Position = u_MvpMatrix * a_Position;\n' +
+  '  v_PositionFromLight = u_MvpMatrixFromLight * a_Position;\n' +
+  '  v_Color = a_Color;\n' +
+  '}\n';
+var FSHADER_SOURCE =
+  '#ifdef GL_ES\n' +
+  'precision mediump float;\n' +
+  '#endif\n' +
+  'uniform sampler2D u_ShadowMap;\n' +
+  'varying vec4 v_PositionFromLight;\n' +
+  'varying vec4 v_Color;\n' +
+  //从rgba这四个分量重新计算出Z值
+  'float unpackDepth(const in vec4 rgbaDepth) {\n' +
+  '  const vec4 bitShift = vec4(1.0, 1.0/256.0, 1.0/(256.0*256.0), 1.0/(256.0*256.0*256.0));\n' +
+  '  float depth = dot(rgbaDepth, bitShift);\n' + 
+  '  return depth;\n' +
+  '}\n' +
+  'void main() {\n' +
+  '  vec3 shadowCoord = (v_PositionFromLight.xyz/v_PositionFromLight.w)/2.0 + 0.5;\n' +
+  '  vec4 rgbaDepth = texture2D(u_ShadowMap, shadowCoord.xy);\n' +
+  '  float depth = unpackDepth(rgbaDepth);\n' + //重新计算出Z值
+  '  float visibility = (shadowCoord.z > depth + 0.0015) ? 0.7 : 1.0;\n' +
+  '  gl_FragColor = vec4(v_Color.rgb * visibility, v_Color.a);\n' +
+  '}\n';
+var OFFSCREEN_WIDTH = 2048, OFFSCREEN_HEIGHT = 2048;
+var LIGHT_X = 0, LIGHT_Y = 40, LIGHT_Z = 2; 
+function main() {
+    var canvas = document.getElementById('webgl');
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+    var shadowProgram = createProgram(gl, SHADOW_VSHADER_SOURCE, SHADOW_FSHADER_SOURCE);
+    shadowProgram.a_Position = gl.getAttribLocation(shadowProgram, 'a_Position');
+    shadowProgram.u_MvpMatrix = gl.getUniformLocation(shadowProgram, 'u_MvpMatrix');
+    if (shadowProgram.a_Position < 0 || !shadowProgram.u_MvpMatrix) {
+        console.log('Failed to get the storage location of attribute or uniform variable from shadowProgram');
+        return;
+    }
+    var normalProgram = createProgram(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+    normalProgram.a_Position = gl.getAttribLocation(normalProgram, 'a_Position');
+    normalProgram.a_Color = gl.getAttribLocation(normalProgram, 'a_Color');
+    normalProgram.u_MvpMatrix = gl.getUniformLocation(normalProgram, 'u_MvpMatrix');
+    normalProgram.u_MvpMatrixFromLight = gl.getUniformLocation(normalProgram, 'u_MvpMatrixFromLight');
+    normalProgram.u_ShadowMap = gl.getUniformLocation(normalProgram, 'u_ShadowMap');
+    if (normalProgram.a_Position < 0 || normalProgram.a_Color < 0 || !normalProgram.u_MvpMatrix ||
+        !normalProgram.u_MvpMatrixFromLight || !normalProgram.u_ShadowMap) {
+        console.log('Failed to get the storage location of attribute or uniform variable from normalProgram');
+        return;
+    }
+    var triangle = initVertexBuffersForTriangle(gl);
+    var plane = initVertexBuffersForPlane(gl);
+    if (!triangle || !plane) {
+        console.log('Failed to set the vertex information');
+        return;
+    } 
+    var fbo = initFramebufferObject(gl);
+    if (!fbo) {
+        console.log('Failed to initialize frame buffer object');
+        return;
+    }
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, fbo.texture);
+    gl.clearColor(0, 0, 0, 1);
+    gl.enable(gl.DEPTH_TEST);
+    var viewProjMatrixFromLight = new Matrix4(); 
+    viewProjMatrixFromLight.setPerspective(70.0, OFFSCREEN_WIDTH / OFFSCREEN_HEIGHT, 1.0, 200.0);
+    viewProjMatrixFromLight.lookAt(LIGHT_X, LIGHT_Y, LIGHT_Z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    var viewProjMatrix = new Matrix4();   
+    viewProjMatrix.setPerspective(45, canvas.width / canvas.height, 1.0, 100.0);
+    viewProjMatrix.lookAt(0.0, 7.0, 9.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    var currentAngle = 0.0; 
+    var mvpMatrixFromLight_t = new Matrix4(); 
+    var mvpMatrixFromLight_p = new Matrix4(); 
+    var tick = function () {
+        currentAngle = animate(currentAngle);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);            
+        gl.viewport(0, 0, OFFSCREEN_HEIGHT, OFFSCREEN_HEIGHT); 
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); 
+        gl.useProgram(shadowProgram); 
+        drawTriangle(gl, shadowProgram, triangle, currentAngle, viewProjMatrixFromLight);
+        mvpMatrixFromLight_t.set(g_mvpMatrix);
+        drawPlane(gl, shadowProgram, plane, viewProjMatrixFromLight);
+        mvpMatrixFromLight_p.set(g_mvpMatrix);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null); 
+        gl.viewport(0, 0, canvas.width, canvas.height);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); 
+        gl.useProgram(normalProgram);
+        gl.uniform1i(normalProgram.u_ShadowMap, 0);
+        gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_t.elements);
+        drawTriangle(gl, normalProgram, triangle, currentAngle, viewProjMatrix);
+        gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_p.elements);
+        drawPlane(gl, normalProgram, plane, viewProjMatrix);
+        window.requestAnimationFrame(tick, canvas);
+    };
+    tick();
+}
+var g_modelMatrix = new Matrix4();
+var g_mvpMatrix = new Matrix4();
+function drawTriangle(gl, program, triangle, angle, viewProjMatrix) {
+    g_modelMatrix.setRotate(angle, 0, 1, 0);
+    draw(gl, program, triangle, viewProjMatrix);
+}
+function drawPlane(gl, program, plane, viewProjMatrix) {
+    g_modelMatrix.setRotate(-45, 0, 1, 1);
+    draw(gl, program, plane, viewProjMatrix);
+}
+function draw(gl, program, o, viewProjMatrix) {
+    initAttributeVariable(gl, program.a_Position, o.vertexBuffer);
+    if (program.a_Color != undefined) 
+        initAttributeVariable(gl, program.a_Color, o.colorBuffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, o.indexBuffer);
+    g_mvpMatrix.set(viewProjMatrix);
+    g_mvpMatrix.multiply(g_modelMatrix);
+    gl.uniformMatrix4fv(program.u_MvpMatrix, false, g_mvpMatrix.elements);
+    gl.drawElements(gl.TRIANGLES, o.numIndices, gl.UNSIGNED_BYTE, 0);
+}
+function initAttributeVariable(gl, a_attribute, buffer) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.vertexAttribPointer(a_attribute, buffer.num, buffer.type, false, 0, 0);
+    gl.enableVertexAttribArray(a_attribute);
+}
+function initVertexBuffersForPlane(gl) {
+    var vertices = new Float32Array([
+      3.0, -1.7, 2.5, -3.0, -1.7, 2.5, -3.0, -1.7, -2.5, 3.0, -1.7, -2.5
+    ]);
+    var colors = new Float32Array([
+      1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0
+    ]);
+    var indices = new Uint8Array([0, 1, 2, 0, 2, 3]);
+    var o = new Object(); 
+    o.vertexBuffer = initArrayBufferForLaterUse(gl, vertices, 3, gl.FLOAT);
+    o.colorBuffer = initArrayBufferForLaterUse(gl, colors, 3, gl.FLOAT);
+    o.indexBuffer = initElementArrayBufferForLaterUse(gl, indices, gl.UNSIGNED_BYTE);
+    if (!o.vertexBuffer || !o.colorBuffer || !o.indexBuffer) return null;
+    o.numIndices = indices.length;
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    return o;
+}
+function initVertexBuffersForTriangle(gl) {
+    var vertices = new Float32Array([-0.8, 3.5, 0.0, 0.8, 3.5, 0.0, 0.0, 3.5, 1.8]);
+    var colors = new Float32Array([1.0, 0.5, 0.0, 1.0, 0.5, 0.0, 1.0, 0.0, 0.0]);
+    var indices = new Uint8Array([0, 1, 2]);
+    var o = new Object(); 
+    o.vertexBuffer = initArrayBufferForLaterUse(gl, vertices, 3, gl.FLOAT);
+    o.colorBuffer = initArrayBufferForLaterUse(gl, colors, 3, gl.FLOAT);
+    o.indexBuffer = initElementArrayBufferForLaterUse(gl, indices, gl.UNSIGNED_BYTE);
+    if (!o.vertexBuffer || !o.colorBuffer || !o.indexBuffer) return null;
+    o.numIndices = indices.length;
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    return o;
+}
+function initArrayBufferForLaterUse(gl, data, num, type) {
+    var buffer = gl.createBuffer();
+    if (!buffer) {
+        console.log('Failed to create the buffer object');
+        return null;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+    buffer.num = num;
+    buffer.type = type;
+    return buffer;
+}
+function initElementArrayBufferForLaterUse(gl, data, type) {
+    var buffer = gl.createBuffer();
+    if (!buffer) {
+        console.log('Failed to create the buffer object');
+        return null;
+    }
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, data, gl.STATIC_DRAW);
+    buffer.type = type;
+    return buffer;
+}
+function initFramebufferObject(gl) {
+    var framebuffer, texture, depthBuffer;
+    var error = function () {
+        if (framebuffer) gl.deleteFramebuffer(framebuffer);
+        if (texture) gl.deleteTexture(texture);
+        if (depthBuffer) gl.deleteRenderbuffer(depthBuffer);
+        return null;
+    }
+    framebuffer = gl.createFramebuffer();
+    if (!framebuffer) {
+        console.log('Failed to create frame buffer object');
+        return error();
+    }
+    texture = gl.createTexture(); 
+    if (!texture) {
+        console.log('Failed to create texture object');
+        return error();
+    }
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, OFFSCREEN_WIDTH, OFFSCREEN_HEIGHT, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    depthBuffer = gl.createRenderbuffer();
+    if (!depthBuffer) {
+        console.log('Failed to create renderbuffer object');
+        return error();
+    }
+    gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, OFFSCREEN_WIDTH, OFFSCREEN_HEIGHT);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer);
+    var e = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
+    if (gl.FRAMEBUFFER_COMPLETE !== e) {
+        console.log('Frame buffer object is incomplete: ' + e.toString());
+        return error();
+    }
+    framebuffer.texture = texture; 
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+    return framebuffer;
+}
+var ANGLE_STEP = 40;
+var last = Date.now();
+function animate(angle) {
+    var now = Date.now();
+    var elapsed = now - last;
+    last = now;
+    var newAngle = angle + (ANGLE_STEP * elapsed) / 1000.0;
+    return newAngle % 360;
+}
